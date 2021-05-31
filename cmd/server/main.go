6545f143e25f23fe1e38dbd22298971b36ff8c00
Costas Drogos <costas.drogos@gmail.com>
2021-05-31 13:48:54 +0200

    Add a first version of the echo server
    * listens on 127.0.0.1:3000
    * registers a handler for the /api/echo route
    * returns all json that gets posted, errors otherwise.

---
+++ cmd/server/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"net/http"
+	"io/ioutil"
+	"encoding/json"
+	//"fmt"
+)
+
+func main () {
+	listen := "127.0.0.1:3000"
+
+	go startHandler(listen)
+
+	select {}
+}
+
+func startHandler(listen string) {
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/api/echo", echoServer)
+	http.ListenAndServe(listen, mux)
+}
+
+
+func echoServer (w http.ResponseWriter, r *http.Request) {
+
+	content, err := ioutil.ReadAll(r.Body)
+
+	//fmt.Printf(string(content))
+	if err != nil {
+		http.Error(w, "error while reading body", http.StatusBadRequest)
+		return
+	}
+
+	var received map[string]string // cast all as strings
+	errj := json.Unmarshal([]byte(content), &received)
+	if errj != nil {
+		http.Error(w, "invalid json", http.StatusBadRequest)
+		return
+	}
+
+	if received == nil {
+		http.Error(w, "invalid json", http.StatusBadRequest)
+		return
+	}
+
+	c, _ := json.Marshal(received)
+	w.Write(c)
+}
