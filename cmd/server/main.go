e82a8bc43a3be0bfba0b336df1bc42ed019383b3
Costas Drogos <costas.drogos@gmail.com>
2021-05-31 16:01:34 +0200

    Add some rudimentary logging
    for any request we get.

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -5,7 +5,7 @@ import (
 	"io/ioutil"
 	"net/http"
 	"strings"
-	//"fmt"
+	"log"
 )
 
 func main() {
@@ -30,11 +30,13 @@ func echoServer(w http.ResponseWriter, r *http.Request) {
 	case http.MethodPut:
 	default:
 		http.Error(w, "only POST/PUT are allowed", http.StatusMethodNotAllowed)
+		log.Printf("%v [%v] %v %v", http.StatusMethodNotAllowed, r.RemoteAddr, "/api/echo", "Error: only POST/PUT are allowed")
 		return
 	}
 
 	if strings.ToLower(r.Header.Get("Content-Type")) != "application/json" {
 		http.Error(w, "Content-Type must be set to application/json", http.StatusBadRequest)
+		log.Printf("%v [%v] %v %v", http.StatusBadRequest, r.RemoteAddr, "/api/echo", "Error: Content-Type must be set to application/json")
 		return
 	}
 
@@ -43,6 +45,7 @@ func echoServer(w http.ResponseWriter, r *http.Request) {
 	//fmt.Printf(string(content))
 	if err != nil {
 		http.Error(w, "error while reading body", http.StatusBadRequest)
+		log.Printf("%v [%v] %v %v", http.StatusBadRequest, r.RemoteAddr, "/api/echo", "Error: while reading body")
 		return
 	}
 
@@ -50,21 +53,25 @@ func echoServer(w http.ResponseWriter, r *http.Request) {
 	errj := json.Unmarshal([]byte(content), &received)
 	if errj != nil {
 		http.Error(w, "invalid json", http.StatusBadRequest)
+		log.Printf("%v [%v] %v %v", http.StatusBadRequest, r.RemoteAddr, "/api/echo", "Error: invalid json")
 		return
 	}
 
 	if received == nil {
 		http.Error(w, "invalid json", http.StatusBadRequest)
+		log.Printf("%v [%v] %v %v", http.StatusBadRequest, r.RemoteAddr, "/api/echo", "Error: invalid json")
 		return
 	}
 
 	echoed, found := received["echoed"]
 	if found && echoed == "true" {
 		http.Error(w, "echoed cannot be set", http.StatusBadRequest)
+		log.Printf("%v [%v] %v %v", http.StatusBadRequest, r.RemoteAddr, "/api/echo", "Error: echoed cannot be set to true")
 	} else {
 		received["echoed"] = "true"
 		c, _ := json.Marshal(received)
 		w.Header().Add("Content-Type", "application/json")
 		w.Write(c)
+		log.Printf("%v [%v] %v %v", http.StatusOK, r.RemoteAddr, "/api/echo", "OK: echoed back")
 	}
 }
