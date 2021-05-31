004496fb51f4b625782c50b29b1683a48f0d297d
Costas Drogos <costas.drogos@gmail.com>
2021-05-31 14:48:07 +0200

    Test: add Content-Type tests, echoed existence
    Added Content-Type to all of our tests. While there, test also that
    echoed is present in our server's reply.

--- cmd/server/main_test.go
+++ cmd/server/main_test.go
@@ -2,6 +2,8 @@
 package main
 
 import (
+	"io/ioutil"
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -14,6 +16,7 @@ func TestEchoServer(t *testing.T) {
 		name string
 		data string
 		method string
+		ctype string
 		statusExpected int
 	}{
 
@@ -21,36 +24,49 @@ func TestEchoServer(t *testing.T) {
 			name: "Test Post Valid Json",
 			data: `{"username": "xyz", "upload":"xyz"}`,
 			method: "POST",
+			ctype: "application/json",
 			statusExpected: http.StatusOK,
 		},
 		{
 			name: "Test Put Valid Json",
 			data: `{"username": "xyz", "upload":"xyz"}`,
 			method: "PUT",
+			ctype: "application/json",
 			statusExpected: http.StatusOK,
 		},
 		{
 			name: "Test Get Json",
 			data: "{}",
 			method: "GET",
+			ctype: "application/json",
 			statusExpected: http.StatusMethodNotAllowed,
 		},
 		{
 			name: "Test Post Valid Json but with echoed false",
 			data: `{"username": "xyz", "upload":"xyz", "echoed":"false"}`,
 			method: "POST",
+			ctype: "application/json",
 			statusExpected: http.StatusOK,
 		},
 		{
 			name: "Test Post Valid Json but with echoed true",
 			data: `{"username": "xyz", "upload":"xyz", "echoed":"true"}`,
 			method: "POST",
+			ctype: "application/json",
+			statusExpected: http.StatusBadRequest,
+		},
+		{
+			name: "Test Post Valid Json but with erroneous content-type",
+			data: `{"username": "xyz", "upload":"xyz"}`,
+			method: "POST",
+			ctype: "application/xml",
 			statusExpected: http.StatusBadRequest,
 		},
 	}
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
 			req, _ := http.NewRequest(test.method, "http://127.0.0.1:3000/api/echo", strings.NewReader(test.data))
+			req.Header.Set("Content-Type", test.ctype)
 			w := httptest.NewRecorder()
 
 			echoServer(w, req)
@@ -60,6 +76,17 @@ func TestEchoServer(t *testing.T) {
 
 			if resp.StatusCode != test.statusExpected {
 				t.Fatalf("Status Code: Expected %v but got %v", test.statusExpected, resp.StatusCode)
+			}
+
+			if test.statusExpected == http.StatusOK { // check content if we get a 200
+				body, _ := ioutil.ReadAll(resp.Body)
+				var received map[string]interface{}
+				json.Unmarshal([]byte(body), &received)
+
+				v, found := received["echoed"]
+				if !found || v != "true" {
+					t.Fatal("Echoed not found")
+				}
 			}
 		})
 	}
